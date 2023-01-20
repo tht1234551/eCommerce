60bd800cdb93670b96fe14c66c9d16ece9c59ef1
Jung <tht1234551@gmail.com>
2023-01-20 16:59:44 +0900

    #12 마크업 그리는중 임시 커밋

--- src/main/webapp/src/component/Header.tsx
+++ src/main/webapp/src/component/Header.tsx
@@ -2,55 +2,56 @@ import logo from '../logo.svg'
 import {BsSearch, BsPerson} from 'react-icons/bs'
 import {Link} from "react-router-dom";
 import {LinkContainer} from "react-router-bootstrap";
-import {Nav, Container, Row, Col} from "react-bootstrap";
+import {Nav, Container, Row, Col, Button} from "react-bootstrap";
 
 function Header() {
-    const navList = [
-        {title: 'Home', link: '/'},
-        {title: 'Pub', link: '/pub'},
-        {title: 'Test', link: '/test'},
+    type nav = {
+        title: string;
+        to: string;
+    }
+
+    const navs : nav[] = [
+        {title: 'Home', to: '/'},
+        {title: 'Pub', to: '/pub'},
+        {title: 'Test', to: '/test'},
     ];
 
 
     return (
-        <header>
-            <Container>
-                <Row>
-                    <Row className="align-items-center">
-                        <Col xs={2} lg={2}>
-                            <img src={logo} className="img-fluid" width={150} alt=""/>
-                        </Col>
-                        <Col xs={10} lg={7}>
-                            <div className="input-group">
-                                <input type="text" className="form-control" placeholder="Find your item"/>
-                                <div className="input-group-append">
-                                    <button className="btn btn-primary" type="button">
-                                        <BsSearch/>
-                                    </button>
-                                </div>
+        <Container as="header">
+            <Row>
+                <Row className="align-items-center">
+                    <Col xs={2} lg={2}>
+                        <img src={logo} className="img-fluid" width={150} alt=""/>
+                    </Col>
+                    <Col xs={10} lg={7}>
+                        <div className="input-group">
+                            <input type="text" className="form-control" placeholder="Find your item"/>
+                            <div className="input-group-append">
+                                <Button variant="primary">
+                                    <BsSearch/>
+                                </Button>
                             </div>
-                        </Col>
-                        <Col lg={3}>
-                            <div className="row">
-                                <ul className="nav nav-pills text-small" style={{paddingRight: 0}}>
-                                    {navList.map(({title, link}) =>
-                                        <li className="col-12 col-md-auto">
-                                            <Link to={link} className="nav-link text-secondary">
-                                                <BsPerson className="d-block mx-auto" size="1.5rem"/>
-                                                {title}
-                                            </Link>
-
-                                        </li>
-                                    )}
-
-                                </ul>
-                            </div>
-                        </Col>
-                    </Row>
+                        </div>
+                    </Col>
+                    <Col lg={3}>
+                        <Row>
+                            <ul className="nav nav-pills text-small" style={{paddingRight: 0}}>
+                                {navs.map(({title, to}) =>
+                                    <li key={to} className="col-12 col-md-auto">
+                                        <Link to={to} className="nav-link text-secondary">
+                                            <BsPerson className="d-block mx-auto" size="1.5rem"/>
+                                            {title}
+                                        </Link>
+                                    </li>
+                                )}
+                            </ul>
+                        </Row>
+                    </Col>
                 </Row>
-                <Row>2</Row>
-            </Container>
-        </header>
+            </Row>
+            <Row>2</Row>
+        </Container>
     )
 }
 

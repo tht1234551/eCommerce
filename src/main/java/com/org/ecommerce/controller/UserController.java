6a4763fa8063bb39fb9124b640f86619953ac3fc
Jung <tht1234551@gmail.com>
2023-02-01 23:09:10 +0900

    #14 Spring rest data 추가

--- src/main/java/com/org/ecommerce/controller/UserController.java
+++ src/main/java/com/org/ecommerce/controller/UserController.java
@@ -4,42 +4,43 @@ import com.org.ecommerce.domain.User;
 import com.org.ecommerce.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@RestController
-@RequestMapping("/users")
+@RepositoryRestController
+//@RequestMapping("/users")
 @Slf4j
 @AllArgsConstructor
 public class UserController {
 
     private UserService service;
 
-    @GetMapping
-    public List<User> getAccounts() {
-        return service.readAll();
-    }
-
-    @GetMapping(path="/{email}")
-    public User getAccount(@PathVariable String email) {
-        log.info(email);
-        return service.read(email);
-    }
-
-    @PostMapping
-    public User joinAccount(User user) {
-        return service.insert(user);
-    }
-
-    @PatchMapping
-    public User updateAccount(User user) {
-        return service.update(user);
-    }
-
-    @DeleteMapping
-    public Long deleteAccount(String email) {
-        return service.delete(email);
-    }
+//    @GetMapping
+//    public List<User> getAccounts() {
+//        return service.readAll();
+//    }
+//
+//    @GetMapping(path="/{email}")
+//    public User getAccount(@PathVariable String email) {
+//        log.info(email);
+//        return service.read(email);
+//    }
+//
+//    @PostMapping
+//    public User joinAccount(User user) {
+//        return service.insert(user);
+//    }
+//
+//    @PatchMapping
+//    public User updateAccount(User user) {
+//        return service.update(user);
+//    }
+//
+//    @DeleteMapping
+//    public Long deleteAccount(String email) {
+//        return service.delete(email);
+//    }
 }

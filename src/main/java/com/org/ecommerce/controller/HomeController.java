f672531f4d0d74da2e2477933ea53e70149b56d8
Jung <tht1234551@gmail.com>
2023-07-29 21:23:52 +0900

    test

--- src/main/java/com/org/ecommerce/controller/HomeController.java
+++ src/main/java/com/org/ecommerce/controller/HomeController.java
@@ -6,15 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping
 @Slf4j
 @AllArgsConstructor
 public class HomeController {
-//    @SuppressWarnings("SpringMVCViewInspection")
-//    @GetMapping("/")
-//    public String home(String[] cars) {
-//        return "index.html";
-//    }
+    @SuppressWarnings("SpringMVCViewInspection")
+    @ResponseBody
+    public String home(String[] cars) {
+        return "test";
+    }
 }
